e6356502a4c6e1a3c9085fa1505773c6602f1e79
sorblack-x <sorblack123@gmail.com>
2016-02-10 20:09:03 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -229,7 +229,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {150575718,151927371,177500661,103987264},--Sudo users
+    sudo_users = {134843111,171481453,177500661},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     
