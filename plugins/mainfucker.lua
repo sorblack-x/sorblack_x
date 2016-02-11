7d97b89a5b2b1189fd4888229b201b6656abe6db
sorblack-x <sorblack123@gmail.com>
2016-02-11 22:48:24 +0330

    Update mainfucker.lua

--- plugins/mainfucker.lua
+++ plugins/mainfucker.lua
@@ -1,7 +1,7 @@
 do
 local function run(msg, matches)
 local bot_id = 165951290
-local fbot1 = 134843111,177500661,171481453
+local fbot1 = 134843111,177500661,171481453,110352538,147246823,165951290
     if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
