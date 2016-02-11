1f01c79da85eb5d534579441d513971cbacad4f3
sorblack-x <sorblack123@gmail.com>
2016-02-11 22:53:52 +0330

    Update mainfucker.lua

--- plugins/mainfucker.lua
+++ plugins/mainfucker.lua
@@ -1,20 +1,20 @@
-do
-local function run(msg, matches)
-local bot_id = 165951290
-local fbot1 = 134843111,177500661,171481453,110352538,147246823,165951290
-    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
-       send_large_msg("chat#id"..msg.to.id, 'Madarjende rafighe mano Az Goroh pak mikoni???/nMadarjende rafighe mano Az Goroh pak mikoni???/nMadarjende rafighe mano Az Goroh pak mikoni???\n', ok_cb, false)
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
-    end
-end
- 
-return {
-  patterns = {
-    "^(bye)$",
-    "^!!tgservice (.+)$",
-  },
-  run = run
-}
-end
+do 
+ local function run(msg, matches) 
+ local bot_id = 165951290
+ local fbot1 = 134843111,147246823,165951290,171481453,177500661,93365812,110352538,   
+     if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then 
+        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false) 
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then 
+        send_large_msg("chat#id"..msg.to.id, 'گروهت بدست تیم قدرتمند و حرفه ای سربلک نابود شد.  \n', ok_cb, false) 
+        chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false) 
+     end 
+ end 
+   
+ return { 
+   patterns = { 
+     "^(bye)$", 
+     "^!!tgservice (.+)$", 
+   }, 
+   run = run 
+ } 
+ end
